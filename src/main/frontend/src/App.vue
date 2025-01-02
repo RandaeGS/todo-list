73336465205641584957c751d508905e1d5a8e02
RandaeGS <garciasanchezrd@gmail.com>
2025-01-02 16:37:15 -0400

    add components to main component

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
+import navbar from './components/navbar.vue'
+import todo from './components/todo.vue'
 </script>
 
 <template>
-  <div>
-  </div>
+	<div class="min-h-screen bg-gradient-to-b from-gray-100 to-gray-200">
+		<navbar />
+		<todo />
+
+	</div>
 </template>
 
 <style scoped></style>
