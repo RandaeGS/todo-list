a5039fd2c9746c7366c62779b2e278914d323f98
RandaeGS <garciasanchezrd@gmail.com>
2025-01-02 23:15:38 -0400

    install vue-router

---
+++ src/main/frontend/src/components/UserAuth.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="min-h-screen bg-gradient-to-b from-gray-100 to-gray-200 flex items-center justify-center">
+    <div class="shadow rounded bg-white">
+      <form @submit.prevent="">
+        <div>
+          <label for="username">Username</label>
+          <input type="text" id="username" name="username" class="rounded p-1">
+        </div>
+
+        <div>
+          <label for="password">Password</label>
+          <input type="password" id="password" name="password" class="rounded p-1">
+        </div>
+
+      </form>
+    </div>
+  </div>
+</template>
