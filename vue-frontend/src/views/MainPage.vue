4dc822a1f3a32f7f9e55f5c0a51e3f715c96931f
RandaeGS <garciasanchezrd@gmail.com>
2025-01-08 17:12:00 -0400

    init create task

--- vue-frontend/src/views/MainPage.vue
+++ vue-frontend/src/views/MainPage.vue
@@ -3,13 +3,22 @@
 </script>
 
 <template>
-  <div class="bg-gradient-to-b from-slate-100 to-slate-200">
+  <div class="">
     <nav class="bg-slate-600 shadow p-4">
       <h1 class="text-white font-bold text-center text-2xl">Todo List</h1>
-
     </nav>
 
-    <main>
+    <main class="flex flex-col items-center justify-center">
+      <div class="w-full max-w-xl bg-slate-600 mt-4 p-4">
+        <form>
+          <h1 class="text-white text-center text-2xl font-semibold">Create Task</h1>
+          <div class="mt-4 flex">
+            <input type="text" name="inputTask" id="inputTask" class="border rounded shadow p-1 grow">
+            <button class="bg-blue-400 hover:bg-blue-500 rounded transition-colors p-2 text-white mx-2">Create</button>
+          </div>
+        </form>
+
+      </div>
 
     </main>
 
