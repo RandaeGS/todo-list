70402a11e9515f7e36f0e0d49bc35f38106ff711
RandaeGS <garciasanchezrd@gmail.com>
2025-01-08 16:19:01 -0400

    init mainpage view

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import AuthView from './views/AuthPage.vue'
+import MainPage from './views/MainPage.vue'
 </script>
 
 <template>
   <div>
+    <MainPage />
 
-    <AuthView />
   </div>
 
 
---
+++ vue-frontend/src/views/MainPage.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="bg-gradient-to-b from-slate-100 to-slate-200">
+    <nav class="bg-slate-600 shadow p-4">
+      <h1 class="text-white font-bold text-center text-2xl">Todo List</h1>
+
+    </nav>
+
+    <main>
+
+    </main>
+
+  </div>
+</template>
