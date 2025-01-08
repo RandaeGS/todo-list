96485b25a7d1c877a8b18516f0dfa7b79886bc12
RandaeGS <garciasanchezrd@gmail.com>
2025-01-08 08:49:19 -0400

    init vue frontend correctly

---
+++ vue-frontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import AuthView from './views/AuthPage.vue'
+</script>
+
+<template>
+  <div>
+
+    <AuthView />
+  </div>
+
+
+</template>
---
+++ vue-frontend/src/views/AuthPage.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="min-h-screen items-center justify-center flex flex-col bg-gradient-to-b from-gray-50 to-gray-100">
+
+    <div class="shadow-lg bg-white p-4 rounded w-full max-w-md">
+      <h1 class="font-bold text-2xl text-center">Welcome</h1>
+      <p class="text-center my-2 font-semibold text-gray-900">Todo List Frontend - Vue</p>
+      <form class="">
+        <div class="my-4">
+          <label for="username" class="block font-semibold text-sm mb-2 text-gray-900">Username</label>
+          <input type="text" name="username" id="username" class="w-full rounded border shadow p-2"
+            placeholder="Username">
+        </div>
+
+        <div class="my-4 w-full">
+          <label for="password" class="block font-semibold text-sm mb-2 text-gray-900">Password</label>
+          <input type="password" name="password" id="password" class="w-full rounded border shadow p-2"
+            placeholder="Enter your password">
+        </div>
+
+        <button type="submit"
+          class="w-full bg-blue-400 hover:bg-blue-500 transition-colors rounded px-2 py-2 text-white">Login</button>
+
+        <div class="flex justify-center mt-3">
+          <p>Don't have a user?</p>
+          <p class="text-blue-400 mx-2">Sign up now</p>
+
+        </div>
+      </form>
+    </div>
+
+  </div>
+</template>
