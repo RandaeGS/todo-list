de44c7819a2a6a054dad90c5b8fe976a561be3ff
RandaeGS <garciasanchezrd@gmail.com>
2025-01-13 18:38:37 -0400

    implement securityFilterChain configuration

--- spring-backend/src/main/java/com/example/springbackend/configuration/WebSecurityConfig.java
+++ spring-backend/src/main/java/com/example/springbackend/configuration/WebSecurityConfig.java
@@ -1,16 +1,21 @@
 package com.example.springbackend.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
@@ -18,8 +23,15 @@ public class WebSecurityConfig {
 	@Autowired
 	public MyUserDetailsService userDetailsService;
 
-	public SecurityFilterChain securityFilterChain(HttpSecurity http){
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(AbstractHttpConfigurer::disable).
+						authorizeHttpRequests(request -> request.
+										requestMatchers("/auth/**").permitAll()
+										.anyRequest().authenticated())
+						.httpBasic(Customizer.withDefaults()).
+						sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
+		return http.build();
 	}
 
 	@Bean
