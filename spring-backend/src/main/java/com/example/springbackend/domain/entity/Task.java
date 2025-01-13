2a8e31b457cf59355b321192c0b352e57edbb622
RandaeGS <garciasanchezrd@gmail.com>
2025-01-13 18:15:23 -0400

    create task entity

---
+++ spring-backend/src/main/java/com/example/springbackend/domain/entity/Task.java
@@ -0,0 +1,28 @@
+package com.example.springbackend.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Task {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String description;
+	private boolean isCompleted;
+	private boolean isDeleted;
+
+	public Task(String description) {
+		this.description = description;
+		this.isCompleted = false;
+		this.isDeleted = false;
+	}
+}
