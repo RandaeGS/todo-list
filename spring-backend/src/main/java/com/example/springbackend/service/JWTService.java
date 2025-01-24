95ccbf0b7c7c56ac4b714412fcaaffa476e71997
RandaeGS <garciasanchezrd@gmail.com>
2025-01-24 12:54:56 -0400

    implement jwt key generation on JWTService

--- spring-backend/src/main/java/com/example/springbackend/service/JWTService.java
+++ spring-backend/src/main/java/com/example/springbackend/service/JWTService.java
@@ -1,7 +1,48 @@
 package com.example.springbackend.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JWTService {
+
+	private String secretKey;
+
+	public JWTService() throws NoSuchAlgorithmException {
+		KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+		SecretKey key = keyGen.generateKey();
+		secretKey = Base64.getEncoder().encodeToString(key.getEncoded());
+	}
+
+	public String generateToken(String username) {
+		Map<String, Object> claims = new HashMap<>();
+		return createToken(claims, username);
+	}
+
+	public String createToken(Map<String, Object> claims, String username) {
+		return Jwts.builder()
+						.claims(claims)
+						.subject(username)
+						.issuedAt(new Date())
+						.expiration(new Date(System.currentTimeMillis() + 1000 * 60 *30))
+						.signWith(getSignKey())
+						.compact();
+	}
+
+	public Key getSignKey(){
+		byte[] bytes = Decoders.BASE64.decode(secretKey);
+		return Keys.hmacShaKeyFor(bytes);
+	}
 }
