e573ea46d5e0121274f33b00fa76703c2ba71b16
RandaeGS <garciasanchezrd@gmail.com>
2025-01-24 16:37:58 -0400

    change order

--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -1,14 +1,14 @@
 import './index.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router/index.js'
-import { createPinia } from 'pinia'
 
-const app = createApp(App)
 const pinia = createPinia()
+const app = createApp(App)
 
-app.use(router)
 app.use(pinia)
+app.use(router)
 
 app.mount('#app')
