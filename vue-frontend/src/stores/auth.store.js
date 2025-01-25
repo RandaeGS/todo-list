a5a08e5f99f71050b90c4895c58e1a5bfd0e7e47
RandaeGS <garciasanchezrd@gmail.com>
2025-01-24 23:46:49 -0400

    add payload and expiration methods

--- vue-frontend/src/stores/auth.store.js
+++ vue-frontend/src/stores/auth.store.js
@@ -8,6 +8,28 @@ export const useAuthStore = defineStore('auth', {
     token: (state) => {
       return state.userJwt
     },
+    tokenPayload: (state) => {
+      if (!state.userJwt) {
+        return null
+      }
+      try {
+        const payload = state.userJwt.split(.)[1];
+        return JSON.parse(atob(payload))
+      } catch (error) {
+        console.error('Error decoding token: ', error)
+        return null
+      }
+    },
+    isTokenExpired: (state) => {
+      const payload = state.tokenPayload
+      if (!payload) {
+        return null
+      }
+      const expireTime = payload.exp * 1000
+      const currentTime = Date.now()
+
+      return currentTime >= expireTime
+    }
   },
   actions: {
     async login(username, password) {
