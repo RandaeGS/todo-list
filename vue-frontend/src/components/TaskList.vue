be37b09e073b2e30f6204ce541896a887d7781d5
RandaeGS <garciasanchezrd@gmail.com>
2025-01-30 19:34:20 -0400

    add filter options to task list

--- vue-frontend/src/components/TaskList.vue
+++ vue-frontend/src/components/TaskList.vue
@@ -1,6 +1,37 @@
 <template>
   <div class="w-full mt-6 max-w-xl bg-slate-600 p-4">
-    <h1 class="text-white text-center text-2xl font-semibold ">Task List</h1>
+    <div class="flex justify-between items-center mb-4">
+      <div class="flex-1"></div>
+      <h1 class="text-white text-center text-2xl font-semibold flex-1">Task List</h1>
+      <div class="text-white flex items-center gap-1 flex-1 justify-end">
+        <div class="bg-slate-700 p-1 rounded-lg flex gap-1"> <!-- Contenedor para los botones -->
+          <button @click="filter = 'all'" :class="[
+            'px-3 py-1.5 rounded-md text-sm font-medium transition-all duration-200',
+            filter === 'all'
+              ? 'bg-blue-500 text-white shadow-sm'
+              : 'text-gray-300 hover:bg-slate-600'
+          ]">
+            All
+          </button>
+          <button @click="filter = 'complete'" :class="[
+            'px-3 py-1.5 rounded-md text-sm font-medium transition-all duration-200',
+            filter === 'complete'
+              ? 'bg-blue-500 text-white shadow-sm'
+              : 'text-gray-300 hover:bg-slate-600'
+          ]">
+            Complete
+          </button>
+          <button @click="filter = 'incomplete'" :class="[
+            'px-3 py-1.5 rounded-md text-sm font-medium transition-all duration-200',
+            filter === 'incomplete'
+              ? 'bg-blue-500 text-white shadow-sm'
+              : 'text-gray-300 hover:bg-slate-600'
+          ]">
+            Incomplete
+          </button>
+        </div>
+      </div>
+    </div>
 
     <div v-for="task in list" :key="task.id">
       <div class="flex rounded shadow m-4 p-2 bg-slate-100 justify-between hover:shadow-2xl">
@@ -20,16 +51,25 @@ import { ref, onMounted } from "vue";
 import axiosInstance from '@/plugins/axios.js'
 
 const list = ref()
+const filter = ref('all')
 
 onMounted(async () => {
+  fetchTasks()
+})
+
+const fetchTasks = async () => {
   try {
-    const response = await axiosInstance.get('/tasks')
-    console.log("Fetch: ", response)
+    const response = await axiosInstance.get('/tasks', {
+      params: {
+        filter: filter.value
+      }
+    })
     list.value = response.data
   } catch (error) {
     console.error("Error fetching tasks: ", error);
   }
-})
+
+};
 
 const toggleTaskStatus = async (taskId) => {
   try {
